f2f3897938c9527949d6241920610bf427d71e45
Rafael Mensen <rafaelmensen0402@gmail.com>
2025-08-26 20:16:12 -0300

    chore: base do Vite para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: { port: 5173 }
+// })
+
+
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  base: '/mrm_ar/', // << IMPORTANTE
 })
